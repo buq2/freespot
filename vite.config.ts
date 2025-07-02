f5689a32cf33b52e869c28d1fbffa691875fde3d
Matti Jukola <buq2@buq2.com>
2025-07-02 12:43:34 +0300

    Smaller chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,42 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 400,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // React core
+          'vendor-react': ['react', 'react-dom'],
+          
+          // Material-UI core components
+          'vendor-mui-core': [
+            '@mui/material',
+            '@emotion/react',
+            '@emotion/styled'
+          ],
+          
+          // Material-UI icons (separate due to size)
+          'vendor-mui-icons': ['@mui/icons-material'],
+          
+          // Material-UI date pickers
+          'vendor-mui-pickers': ['@mui/x-date-pickers'],
+          
+          // Map and geolocation libraries
+          'vendor-maps': [
+            'leaflet',
+            'react-leaflet',
+            'geolib'
+          ],
+          
+          // Utility libraries
+          'vendor-utils': [
+            'date-fns',
+            'axios',
+            'localforage'
+          ]
+        }
+      }
+    }
+  }
 })
